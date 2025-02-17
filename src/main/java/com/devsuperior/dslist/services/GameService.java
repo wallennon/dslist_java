40f16965b014d0cdfc9e23fc386df5488d34bfc4
wallennon <wallennon@gmail.com>
2025-02-17 20:16:56 -0300

    Add GameList (controller, Service, repository e DTO

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,10 +31,14 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x  -> new GameMinDTO(x)).toList();
 
-
         /*didaticamente atribui-se uma lista à variável para receber os parâmetros.*/
 
+    }
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
 }
